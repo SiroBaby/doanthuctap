5da355859506c240bbaeb8341249e8a2158adfdd
SiroBaby <ngocphat076@gmail.com>
2025-02-25 20:05:50 +0700

    Add AnotherTopBar and ProductCard components; update imports in HomePage

--- frontend/my-app/app/(home)/page.tsx
+++ frontend/my-app/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 //import Image from 'next/image';
 //import { FaTicketAlt, FaTshirt, FaShoes, FaDesktop, FaMobile, FaClock, FaMicrochip, FaMousePointer } from 'react-icons/fa';
-import AnotherTopBar from '../component/layout/AnotherTopBar';
+import AnotherTopBar from '../components/layout/AnotherTopBar';
 //import ProductCard from '../components/layout/ProductCard'; 
 import "../globals.css";
 
@@ -9,11 +9,11 @@ const HomePage: React.FC = () => {
   return (
     <div className="w-full">
       <div>
-      <AnotherTopBar/>
+        <AnotherTopBar />
       </div>
-      
+
     </div>
-    
+
   );
 };
 
